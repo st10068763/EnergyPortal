015cbf84c4e45cbd6efba3495568899290552796
st10068763 <anterozacarias007@gmail.com>
2024-05-23 12:18:56 +0200

    Added the commuincation hub and a table to store the messages and the message details.

---
+++ ProgPoeAgriEnergyPortal/CommunicationHub.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CommunicationHub.aspx.cs" Inherits="ProgPoeAgriEnergyPortal.CommunicationHub" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Communication Hub</title>
+    <style>
+        .chat-container {
+            display: flex;
+            flex-direction: column;
+            height: 80vh;
+            width: 50%;
+            margin: 0 auto;
+            border: 1px solid #ccc;
+            padding: 10px;
+        }
+        .chat-box {
+            flex: 1;
+            overflow-y: scroll;
+            border: 1px solid #ccc;
+            margin-bottom: 10px;
+            padding: 10px;
+        }
+        .message-input {
+            display: flex;
+        }
+        .message-input textarea {
+            flex: 1;
+            padding: 5px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+        }
+        .message-input button {
+            padding: 5px 10px;
+            margin-left: 10px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="chat-container">
+            <div class="chat-box" id="chatBox" runat="server">
+                <!-- Messages will be displayed here -->
+            </div>
+            <div class="message-input">
+                <asp:TextBox ID="txtMessage" runat="server" TextMode="MultiLine" Rows="2" placeholder="Type your message..."></asp:TextBox>
+                <asp:Button ID="btnSend" runat="server" Text="Send" OnClick="btnSend_Click" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ProgPoeAgriEnergyPortal/CommunicationHub.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProgPoeAgriEnergyPortal
+{
+    public partial class CommunicationHub : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Check if the user is logged in
+                if (Session["Employee_ID"] == null)
+                {
+                    Response.Redirect("LoginPage.aspx");
+                }
+                else
+                {
+                    LoadMessages();
+                }
+            }
+
+        }
+        /// <summary>
+        /// save the message to the database
+        /// </summary>
+        /// <param name="senderId"></param>
+        /// <param name="receiverId"></param>
+        /// <param name="message"></param>
+        private void SaveMessage(string senderId, string receiverId, string message)
+        {
+            string connectionString = "Data Source=agrisqlserver.database.windows.net;Initial Catalog=AgriEnergyDB;Persist Security Info=True;User ID=st10068763;Password=MyName007";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Messages (SenderId, ReceiverId, MessageText, Timestamp) VALUES (@SenderId, @ReceiverId, @MessageText, @Timestamp)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@SenderId", senderId);
+                cmd.Parameters.AddWithValue("@ReceiverId", receiverId);
+                cmd.Parameters.AddWithValue("@MessageText", message);
+                cmd.Parameters.AddWithValue("@Timestamp", DateTime.Now);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        /// <summary>
+        /// Load the messages from the database
+        /// </summary>
+        private void LoadMessages()
+        {
+
+            string connectionString = "Data Source=agrisqlserver.database.windows.net;Initial Catalog=AgriEnergyDB;Persist Security Info=True;User ID=st10068763;Password=MyName007";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT SenderId, MessageText, Timestamp FROM Messages ORDER BY Timestamp";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<string> messages = new List<string>();
+
+                while (reader.Read())
+                {
+                    string senderId = reader["SenderId"].ToString();
+                    string messageText = reader["MessageText"].ToString();
+                    string timestamp = Convert.ToDateTime(reader["Timestamp"]).ToString("yyyy-MM-dd HH:mm:ss");
+
+                    messages.Add($"<b>{senderId}</b>: {messageText} <i>({timestamp})</i>");
+                }
+
+                chatBox.InnerHtml = string.Join("<br />", messages);
+            }
+        }
+        /// <summary>
+        /// button to send the message
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            // Get the message from the text box
+            string message = txtMessage.Text.Trim();
+            // Get the sender and receiver IDs from the session
+            string senderId = Session["Employee_ID"].ToString(); 
+            string receiverId = Session["Farmer_ID"].ToString(); 
+            
+            if (!string.IsNullOrEmpty(message))
+            {
+                SaveMessage(senderId, receiverId, message);
+                txtMessage.Text = string.Empty;
+                LoadMessages();
+            }
+        }
+    }
+}
